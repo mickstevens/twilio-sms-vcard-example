2d185796da8e40f955f0580e4a3c8d76d92b5c82
plabbett <patrick.labbett@gmail.com>
2015-05-14 07:24:59 +0000

    initial commit

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+	$config['email'] = 'you@example.com'; //where each inbound text gets emailed
+	$config['gravatar_email'] = $config['email']; //so you can use separate one for your profile picture
+	
+	//sign up for a free account at nextcaller.com
+	$config['nextcaller']['username'] = 'apiusername';
+	$config['nextcaller']['password'] = 'apipassword';
+	
---
+++ sms.php
@@ -0,0 +1,55 @@
+<?php
+
+	require 'vendor/autoload.php';
+	require 'config.php';
+
+	use NextCaller\NextCallerClient;
+	$profiles = array();
+	
+	try{
+		$nc = new NextCallerClient(
+			$config['nextcaller']['username'], 
+			$config['nextcaller']['password'], 
+			false
+		);
+		
+		$profiles = $nc->getProfileByPhone(substr($_REQUEST['From'], -10));
+	}
+	catch (\NextCaller\Exception\BadResponseException $e) {
+		//die(var_dump($e));
+	}
+	catch (\NextCaller\Exception\FormatException $e) {
+		//die(var_dump($e));
+	}
+	catch(Exception $e){
+	    //die(var_dump($e));
+	}
+
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<Response>
+    <?php 
+		if(strtolower($_REQUEST['Body']) == 'pic'){
+			?><Message><Media>http://www.gravatar.com/avatar/<?php echo hash('md5', $config['gravatar_email']); ?>?s=480</Media></Message><?php
+		}
+		elseif(strtolower($_REQUEST['Body']) == 'vcard'){
+			?><Message><Media>Patrick_Labbett.vcf</Media></Message><?php
+		}
+		elseif(strtolower($_REQUEST['Body']) == 'hi'){
+				
+			mail(
+				$config['email'], 
+				'Inbound vCard Request', 
+				print_r($profiles, true) .   print_r($_REQUEST, true)
+			);
+	
+			?><Message><Body>
+			Hi, I'm Patrick Labbett! Text "vcard" for my contact info or "pic" to jog your memory. Visit my website at https://labbett.net
+			</Body></Message>
+			<?php
+		}
+		else{
+			//don't respond...
+		}
+		?>
+</Response>
